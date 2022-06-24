84863c87f3e29c9d2f6e7fbba1e33a6631e1e246
Anna Ermolaeva <ermolaeva-ann@mail.ru>
2022-06-24 16:25:50 +0300

    Initial commit

---
+++ app/src/main/java/com/ann/myenglish/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ann.myenglish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
